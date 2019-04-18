d9e2346493d4ddd21e2b3a8c6a41937bb7a7fefe
Jay Jagpal <jay@strangemother.com>
2019-04-18 03:45:23 +0100

    Detect debug mode for dry_run message tests
    If debug is true a real SMS will not send.

--- service/contrib/connect/sms.py
+++ service/contrib/connect/sms.py
@@ -9,16 +9,16 @@ class TextLocal(SessionCallable):
     def created(self, index):
         contact_number = self.data['tel']
         apikey = self.data['apikey']
-        resp =  send_sms(apikey, contact_number,'SMS Verify')
+        resp =  send_sms(apikey, contact_number,'SMS Verify', self.client_space.debug)
         self.log(resp)
 
 # http://api.txtlocal.com/docs/sendsms
-def send_sms(apikey, numbers, message):
+def send_sms(apikey, numbers, message, test=False):
     data =  urllib.parse.urlencode({
         'apikey': apikey,
         'numbers': numbers,
         'message' : message,
-        #'test': True,
+        'test': test,
     })
     data = data.encode('utf-8')
     request = urllib.request.Request("https://api.txtlocal.com/send/?")
